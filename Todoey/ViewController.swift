c7b6d63efc1e0c9ea4dcb409f35534c03e6e385d
Kurtis Hill <kurtis.hill212@gmail.com>
2019-01-08 09:38:21 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kurtis Hill on 1/8/19.
+//  Copyright © 2019 Kurtis Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
